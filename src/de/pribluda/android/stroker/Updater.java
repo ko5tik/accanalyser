0e12d99cbf1eeaab0228889503883231d0e794e0
ko5tik <konstantin.pribluda@gmail.com>
2012-07-24 11:08:22 -0400

    some log tags added

--- src/de/pribluda/android/stroker/Updater.java
+++ src/de/pribluda/android/stroker/Updater.java
@@ -1,5 +1,7 @@
 package de.pribluda.android.stroker;
 
+import android.graphics.Bitmap;
+import android.util.Log;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 
@@ -9,6 +11,7 @@ import static de.pribluda.android.stroker.UpdaterState.*;
  * performs graphics update
  */
 public class Updater implements SurfaceHolder.Callback {
+    public static final String LOG_TAG = "updater";
     final SurfaceHolder surfaceHolder;
     UpdaterState state;
 
@@ -16,6 +19,7 @@ public class Updater implements SurfaceHolder.Callback {
     private int height;
 
     boolean haveSurface = false;
+    private Bitmap field;
 
     public Updater(SurfaceHolder surfaceHolder) {
         this.surfaceHolder = surfaceHolder;
@@ -26,13 +30,17 @@ public class Updater implements SurfaceHolder.Callback {
         while (RUNNING == state) {
             if(RUNNING == state && haveSurface ) {
                 // draw all the stuff  and post on surface
+                Log.d(LOG_TAG,"updating state");
             }
             try {
+                Log.d(LOG_TAG,"sleeping");
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 // just ignore
             }
         }
+
+        Log.d(LOG_TAG,"main loop ready");
     }
 
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
@@ -40,12 +48,15 @@ public class Updater implements SurfaceHolder.Callback {
     }
 
     public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
-
+        Log.d(LOG_TAG,"surface available");
         this.width = width;
         this.height = height;
 
         haveSurface = true;
 
+        // create bitmap with RGB 565, as this is background, we do not care about
+        // transparency
+        field = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
     }
 
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
@@ -58,6 +69,7 @@ public class Updater implements SurfaceHolder.Callback {
         switch (newState) {
             //  start thread ASAP
             case START:
+                Log.d(LOG_TAG,"starting");
                 state = RUNNING;
                 (new Thread(new Runnable() {
                     public void run() {
@@ -67,6 +79,7 @@ public class Updater implements SurfaceHolder.Callback {
                 break;
             // signal thread to stop
             case STOPPED:
+                Log.d(LOG_TAG,"stopping");
                 state = STOPPED;
                 break;
         }
