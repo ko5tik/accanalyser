3c7886d6afa67d898ec7e59e55de2bf845f1a319
ko5tik <konstantin.pribluda@gmail.com>
2012-07-27 10:11:34 -0400

    encapsulated path creation

--- src/de/pribluda/android/stroker/Updater.java
+++ src/de/pribluda/android/stroker/Updater.java
@@ -38,35 +38,47 @@ public class Updater implements SurfaceHolder.Callback {
 
     public void updateState() {
         while (RUNNING == state) {
-            if(RUNNING == state && haveSurface ) {
+            if (RUNNING == state && haveSurface) {
                 // draw all the stuff  and post on surface
 
                 // clear canvas
                 fieldCanvas.drawRGB(0, 0, 0);
                 // draw individual spectral lines starting from the next one
-                for( int i = 0; i < AMOUNT_SPECTRES; i++ )  {
+
+                int step = width / StrokeDetector.WINDOW_SIZE;
+
+                for (int i = 0; i < AMOUNT_SPECTRES; i++) {
                     double[] energy = energies[(i + 1 + energyIndex) % AMOUNT_SPECTRES];
-                    int offset = (AMOUNT_SPECTRES -i) * 5;
-                    int step = width / energy.length;
-
-                    Path path = new Path();
-
-                    path.moveTo(0,offset);
+                    int offset = (AMOUNT_SPECTRES - i) * 5;
+                    Path path = createPath(step, energy, offset);
 
 
                 }
 
-                Log.d(LOG_TAG,"updating state");
+                Log.d(LOG_TAG, "updating state");
             }
             try {
-                Log.d(LOG_TAG,"sleeping");
+                Log.d(LOG_TAG, "sleeping");
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 // just ignore
             }
         }
 
-        Log.d(LOG_TAG,"main loop ready");
+        Log.d(LOG_TAG, "main loop ready");
+    }
+
+    private Path createPath(int step, double[] energy, int offset) {
+        Path path;
+        path = new Path();
+
+        path.moveTo(offset, offset);
+
+        // iterate over energies
+        for (int j = 0; j < energy.length; j++) {
+            path.lineTo(j * step + offset , (float) (energy[j] + offset));
+        }
+        return path;
     }
 
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
@@ -74,7 +86,7 @@ public class Updater implements SurfaceHolder.Callback {
     }
 
     public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
-        Log.d(LOG_TAG,"surface available");
+        Log.d(LOG_TAG, "surface available");
         this.width = width;
         this.height = height;
 
@@ -96,7 +108,7 @@ public class Updater implements SurfaceHolder.Callback {
         switch (newState) {
             //  start thread ASAP
             case START:
-                Log.d(LOG_TAG,"starting");
+                Log.d(LOG_TAG, "starting");
                 state = RUNNING;
                 (new Thread(new Runnable() {
                     public void run() {
@@ -106,7 +118,7 @@ public class Updater implements SurfaceHolder.Callback {
                 break;
             // signal thread to stop
             case STOPPED:
-                Log.d(LOG_TAG,"stopping");
+                Log.d(LOG_TAG, "stopping");
                 state = STOPPED;
                 break;
         }
