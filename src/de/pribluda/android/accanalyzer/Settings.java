bb6806c35cc98646d86fd63ca5cfdb341fa5dcd1
Konstantin Pribluda <konstantin.pribluda@gmail.com>
2012-09-13 22:48:19 +0200

    improving appearance of settings interface

--- src/de/pribluda/android/accanalyzer/Settings.java
+++ src/de/pribluda/android/accanalyzer/Settings.java
@@ -2,6 +2,9 @@ package de.pribluda.android.accanalyzer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Spinner;
+import de.pribluda.android.andject.InjectView;
+import de.pribluda.android.andject.ViewInjector;
 
 /**
  * configuration of application settings
@@ -9,8 +12,23 @@ import android.os.Bundle;
  * @author Konstantin Pribluda
  */
 public class Settings extends Activity {
+
+    @InjectView(id = R.id.sampleRateSpinner)
+    private Spinner sampleRate;
+
+    @InjectView(id = R.id.windowSizeSpinner)
+    private Spinner windowSize;
+
+    @InjectView(id = R.id.updateRateSpinner)
+    private Spinner updateRate;
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+
+        ViewInjector.startActivity(this);
+
+
     }
 }
