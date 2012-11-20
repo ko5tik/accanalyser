cc42ea9727eb6a3af9a738fbf45ce2c795da7258
Konstantin Pribluda <konstantin.pribluda@gmail.com>
2012-11-20 21:52:27 +0100

    fixed settings dialog,  but hit the bug with integers.

--- src/de/pribluda/android/accanalyzer/Settings.java
+++ src/de/pribluda/android/accanalyzer/Settings.java
@@ -1,17 +1,8 @@
 package de.pribluda.android.accanalyzer;
 
-import android.R;
-import android.app.Activity;
-import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.widget.SeekBar;
-import android.widget.TextView;
-import de.pribluda.android.andject.InjectView;
-import de.pribluda.android.andject.ViewInjector;
-
-import java.util.HashMap;
-import java.util.Map;
+import android.preference.PreferenceManager;
 
 /**
  * configuration of application settings
@@ -21,36 +12,21 @@ import java.util.Map;
 public class Settings extends PreferenceActivity {
 
 
-    private static final int[] windowSizesLookup = {16, 32, 64, 128, 256, 512, 1024};
-    private final static Map<Integer, Integer> reverseWindowSizesLookup = new HashMap<Integer, Integer>() {{
-        put(16, 0);
-        put(32, 1);
-        put(64, 2);
-        put(128, 3);
-        put(256, 4);
-        put(512, 5);
-        put(1024, 6);
-    }};
-
-    public static final int[] sensorDelayLookup = {SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI, SensorManager.SENSOR_DELAY_GAME, SensorManager.SENSOR_DELAY_FASTEST};
-    public static final Map<Integer, Integer> reverseSensorDelayLookup = new HashMap() {
-        {
-            put(SensorManager.SENSOR_DELAY_NORMAL, 0);
-            put(SensorManager.SENSOR_DELAY_UI, 1);
-            put(SensorManager.SENSOR_DELAY_GAME, 2);
-            put(SensorManager.SENSOR_DELAY_FASTEST, 3);
-        }
-    };
-    public static final int[] sampleRateTexts = {R.string.sampleRateNormal, R.string.sampleRateUI, R.string.sampleRateGame, R.string.sampleRateFastest};
-
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        PreferenceManager prefMgr = getPreferenceManager();
+        prefMgr.setSharedPreferencesName("my_preferences");
+        prefMgr.setSharedPreferencesMode(MODE_PRIVATE);
+
     }
 
     @Override
     protected void onResume() {
-        super.onResume();    //To change body of overridden methods use File | Settings | File Templates.
+        super.onResume();
     }
+
+
 }
